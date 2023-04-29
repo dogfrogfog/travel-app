2abfffea67c68dc448a07325adaec66d3c23a7d1
dogfrogfog <maksim.hodasevich@gmail.com>
2023-04-29 12:09:33 +0200

    add sign in status to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,6 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs/app-beta";
+import Link from "next/link"
+
 export default function Header() {
   return (
     <header>
@@ -7,7 +10,16 @@ export default function Header() {
                     <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">logo</span>
                 </a>
                 <div className="flex items-center lg:order-2">
-                    <a href="#" className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Log in</a>
+                    <SignedOut>
+                        <Link href="/sign-in" className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">
+                            Log in
+                        </Link>
+                    </SignedOut>
+                    <SignedIn>
+                        <Link href="/profile" className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">
+                            Profile
+                        </Link>
+                    </SignedIn>
                     <button data-collapse-toggle="mobile-menu-2" type="button" className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
                         <span className="sr-only">Open main menu</span>
                         <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>

70e2f535d177eeabde6f2190835ead3655bf563a
dogfrogfog <maksim.hodasevich@gmail.com>
2023-04-29 10:57:59 +0200

    add basic main page setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
 import './globals.css'
 
 export const metadata = {
@@ -12,7 +14,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Header />
+        {children}
+        <Footer />
+      </body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,21 @@
 export default function Home() {
   return (
-    <main>
-      initial page
-    </main>
+    <div>
+      <div className="h-72 w-full bg-slate-50 border-4 border-slate-600 mb-8">
+        <h1>some content 1</h1>
+      </div>
+      <div className="h-72 w-full bg-slate-50 border-4 border-slate-600 mb-8">
+        <h1>some content 2</h1>
+      </div>
+      <div className="h-72 w-full bg-slate-50 border-4 border-slate-600 mb-8">
+        <h1>some content 3</h1>
+      </div>
+      <div className="h-72 w-full bg-slate-50 border-4 border-slate-600 mb-8">
+        <h1>some content 4</h1>
+      </div>
+      <div className="h-72 w-full bg-slate-50 border-4 border-slate-600 mb-4">
+        <h1>some content 5</h1>
+      </div>
+    </div>
   )
 }

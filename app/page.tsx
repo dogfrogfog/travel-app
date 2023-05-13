dba90a0e96164d3816afaefe5490b6ab57d3177f
dogfrogfog <maksim.hodasevich@gmail.com>
2023-05-13 20:02:30 +0200

    add revalidate to page, filters line

--- app/page.tsx
+++ app/page.tsx
@@ -19,3 +19,5 @@ export default async function Home() {
     </div>
   )
 }
+
+export const revalidate = 15;
--- components/Filters.tsx
+++ components/Filters.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { useState } from 'react'
+import { Form, Field } from 'react-final-form'
 import { Input } from '@/components/ui/input'
 import {
     Select,
@@ -8,13 +8,19 @@ import {
     SelectTrigger,
     SelectValue,
   } from "@/components/ui/select"
+import { Button } from "@/components/ui/button"
 import { DatePickerWithRange } from "@/components/ui/datePickerWithRange"
-import { Checkbox } from "@/components/ui/checkbox"
 
 export default function Filters() {
     return (
-        <div className="border-2 border-green-300 mb-12 p-2 rounded">
-            <Select>
+        <div className="bg-slate-600 mb-12 p-2 rounded">
+            <FiltersForm
+                onSubmit={(values: any) => {
+                    console.log(values);
+
+                }}
+            />
+            {/* <Select>
                 <SelectTrigger className="w-[200px]">
                     <SelectValue placeholder="Country, City" />
                 </SelectTrigger>
@@ -23,10 +29,8 @@ export default function Filters() {
                     <SelectItem value="Chicago">Chicago</SelectItem>
                     <SelectItem value="Boston">Boston</SelectItem>
                 </SelectContent>
-            </Select>
-            <DatePickerWithRange onSelect={(v) => console.log(v)} />
-            <Checkbox /> cant figure out
-            <Select>
+            </Select> */}
+            {/* <Select>
                 <SelectTrigger className="w-[200px]">
                     <SelectValue placeholder="Age range" />
                 </SelectTrigger>
@@ -36,8 +40,8 @@ export default function Filters() {
                     <SelectItem value="3">30-40</SelectItem>
                     <SelectItem value="4">40-50+</SelectItem>
                 </SelectContent>
-            </Select>
-            <Select>
+            </Select> */}
+            {/* <Select>
                 <SelectTrigger className="w-[200px]">
                     <SelectValue placeholder="Sex" />
                 </SelectTrigger>
@@ -47,7 +51,38 @@ export default function Filters() {
                     <SelectItem value="3">Custom</SelectItem>
                     <SelectItem value="4">Prefer not to say</SelectItem>
                 </SelectContent>
-            </Select>
+            </Select> */}
+            {/* </div> */}
         </div>
+    )
+}
+
+function FiltersForm({ onSubmit }: { onSubmit: any }) {
+    return (
+        <Form
+            onSubmit={onSubmit}
+            render={({ handleSubmit, values }) => (
+                <form onSubmit={handleSubmit} className='flex justify-between'>
+                    <div className='flex'>
+                        <Field name="title">
+                            {({ input }) => (
+                                <Input {...input} className='rounded bg-slate-300 color-red w-[200px] mr-4' type="text" placeholder='City' />
+                            )}
+                        </Field>
+                        <Field name="dateRange">
+                            {({ meta, input }) => (
+                                <>
+                                    <DatePickerWithRange onSelect={(dateRange) => input.onChange(dateRange)} />
+                                    {meta.error && meta.touched && <span>{meta.error}</span>}
+                                </>
+                            )}
+                        </Field>
+                    </div>
+                    <Button disabled={!values?.title && !values?.dateRange}>
+                        Find trip
+                    </Button>
+                </form>
+            )}
+        />
     )
 }

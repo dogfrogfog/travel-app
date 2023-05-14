ecda0e8742ea6f37711c97d112622cdcd517d347
dogfrogfog <maksim.hodasevich@gmail.com>
2023-05-14 19:16:21 +0200

    add authorDateOfBirth to post to filter by age group

--- app/api/post/createPost/route.ts
+++ app/api/post/createPost/route.ts
@@ -15,6 +15,7 @@ export async function POST(request: Request) {
             data: {
                 ...data,
                 userId: user.id,
+                authorDateOfBirth: user.publicMetadata?.dateOfBirth || null,
             },
         })
         return new NextResponse('ok');
